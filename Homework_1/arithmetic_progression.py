478c5360c92ecfbe8151a7ad08a52c59c5424982
Anush-Sa <sahakyan.anush1@gmail.com>
2021-01-11 02:03:22 +0400

    HW_1.1

---
+++ Homework_1/arithmetic_progression.py
@@ -0,0 +1,9 @@
+a_1 = int(input('Enter first number: '))
+a_2 = int(input('Enter second number: '))
+n = int(input('Enter N: '))
+
+diff = a_2-a_1
+
+a_n = a_1 + (n - 1) * diff 
+
+print('N-th value: ', a_n)
