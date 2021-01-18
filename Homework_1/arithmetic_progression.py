6e0d76cab9af726e4aa3e0fda5b80796282d6c58
Anush-Sa <sahakyan.anush1@gmail.com>
2021-01-18 23:19:30 +0400

    After comments

--- Homework_1/arithmetic_progression.py
+++ Homework_1/arithmetic_progression.py
@@ -2,7 +2,7 @@ a_1 = int(input('Enter first number: '))
 a_2 = int(input('Enter second number: '))
 n = int(input('Enter N: '))
 
-diff = a_2 - clea_1
+diff = a_2 - a_1
 
 a_n = a_1 + (n - 1) * diff 
 
