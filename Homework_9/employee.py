1aa637f9bf6c485e3179d747123170ee5c9f7083
Anush-Sa <sahakyan.anush1@gmail.com>
2021-03-21 14:43:56 +0400

    Homework 9

--- Homework_9/employee.py
+++ Homework_9/employee.py
@@ -25,6 +25,21 @@ class Employee:
     
     def work_email(self):
         return f'{self.first_name.lower()}.{self.last_name.lower()}@company.com'
+    
+    def __eq__(self, other):
+        if (self.first_name, self.last_name) == (other.first_name, other.last_name):
+            return "We have duplicate!"
+        else:
+            return "We don't have duplicated employee information!"
+    
+    def __lt__(self, other):
+        if self.salary<other.salary:
+            return "First employee receives less salary than the other!"
+        else:
+            return "First employee does not receive less salary than the other!"
+        
+    def __add__(self, other):
+        return self.salary + other.salary
 
     def __repr__(self):
         return f'First Name: {self.first_name} \nLast Name: {self.last_name} \nFull Name: {self.full_name()} \nPhone Number: {self.phone_number} \nWork Email: {self.work_email()} \nTrial Passed: {self.trial_passed} \nJoin Date: {self.join_date} \nLeave Date: {self.leave_date} \nSalary: {self.salary} \nGender(M or F): {self.gender}'
@@ -33,6 +48,15 @@ class Employee:
 e1 = Employee ('Davit', 'Tovmasyan', '22.02.2018', phone_number='077 12 34 56', trial_passed=True, salary=15000)
 e2 = Employee ('Tovmas', 'Davtyan', '11.01.2020', leave_date='25.07.2020', salary=45000)
 
+print("Here is our 1st employee information")
+print("____________________________________")
 print(e1)
 print('\n')
+print("Here is our 2nd employee information")
+print("____________________________________")
 print(e2)
+
+print('\nSummary')
+print(e1 == e2)
+print(e1 < e2)
+print("Total salary: ", e1 + e2)
